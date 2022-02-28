b54140354ff87e88ab34a8350723222177b8a1f3
Santiago Chausis <u523982@teco.com.ar>
2022-02-27 22:15:36 -0300

    initial commit

---
+++ assets/data.js
@@ -0,0 +1,33 @@
+export const videos =
+    [
+        {
+            "id": 1,
+            "nombre": "https://www.youtube.com/embed/G72M3qnq58Q",
+        },
+        {
+            "id": 2,
+            "nombre": "https://www.youtube.com/embed/ei7kbQhK1hA",
+        },
+        {
+            "id": 3,
+            "nombre": "https://www.youtube.com/embed/j53JkdiAeXw",
+        },
+        {
+            "id": 4,
+            "nombre": "https://www.youtube.com/embed/_daTfgc4u3k",
+        }, 
+        {
+            "id": 5,
+            "nombre": "https://www.youtube.com/embed/5yx6BWlEVcY",
+        },
+        {
+            "id": 6,
+            "nombre": "https://www.youtube.com/embed/pCuy6U-RVFk",
+        },
+        {
+            "id": 7,
+            "nombre": "https://www.youtube.com/embed/3WBPGAzCcng",
+        }
+    ]
+
+
